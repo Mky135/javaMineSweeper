61a055bc1330b8eefee875ac0a2141c2cb791729
Miguel Flores <mky.flores@hotmail.com>
2018-05-17 13:07:22 -0500

    Initial Commit

---
+++ src/java/game/MainRunner.java
@@ -0,0 +1,18 @@
+package game;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainRunner extends Application
+{
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
